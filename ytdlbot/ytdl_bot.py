178470c251c071679aad3a8af1f7d0bc72511351
#401 <80498933+itsprtyush@users.noreply.github.com>
2021-12-31 08:40:45 +0530

    Update ytdl_bot.py

--- ytdlbot/ytdl_bot.py
+++ ytdlbot/ytdl_bot.py
@@ -12,12 +12,32 @@ import os
 import re
 import tempfile
 import typing
+import asyncio
+
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from pyrogram import Client, filters, types
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from tgbot_ping import get_runtime
+
+########
+from termbot import (
+    AUTH_USERS,
+
+    DELAY_BETWEEN_EDITS,
+    EXEC_CMD_TRIGGER,
+
+    PROCESS_RUNNING,
+
+    aktifperintah,
+    inikerjasaatdirektori
+)
+
+from termbot.helper_funcs.hash_msg import hash_msg
+from termbot.helper_funcs.read_stream import read_stream
+from termbot.helper_funcs.message_editor import MessageEditor
+########
 
 from client_init import create_app
 from config import (AUTHORIZED_USER, ENABLE_CELERY, ENABLE_VIP, OWNER,
@@ -33,8 +53,7 @@ customize_logger(["pyrogram.client", "pyrogram.session.session", "pyrogram.conne
 app = create_app()
 bot_text = BotText()
 
-logging.info("Authorized users are %s", AUTHORIZED_USER)
-
+logging.info("Authorized users are %s", AUTHORIZED_USER) 
 
 def private_use(func):
     def wrapper(client: "Client", message: "types.Message"):
@@ -134,6 +153,42 @@ def settings_handler(client: "Client", message: "types.Message"):
 
     data = get_user_settings(str(chat_id))
     client.send_message(chat_id, bot_text.settings.format(data[1], data[2]), reply_markup=markup)
+
+
+@app.on_message(filters.command([/exec]) & Filters.chat(AUTHORIZED_USER))
+async def execution_cmd_t(client, message):
+    # send a message, use it to update the progress when required
+    status_message = await message.reply_text(PROCESS_RUNNING, quote=True)
+    # get the message from the triggered command
+    cmd = message.text.split(" ", maxsplit=1)[1]
+
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=inikerjasaatdirektori
+    )
+
+    editor = MessageEditor(status_message, cmd)
+    editor.update_process(process)
+
+    aktifperintah[hash_msg(status_message)] = editor
+    await editor.redraw(True)
+    await asyncio.gather(
+        read_stream(
+            editor.update_stdout,
+            process.stdout,
+            DELAY_BETWEEN_EDITS
+        ),
+        read_stream(
+            editor.update_stderr,
+            process.stderr,
+            DELAY_BETWEEN_EDITS
+        )
+    )
+    await editor.cmd_ended(await process.wait())
+    del aktifperintah[hash_msg(status_message)]
 
 
 @app.on_message(filters.command(["vip"]))
